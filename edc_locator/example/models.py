94b439902673dc488a0f133fea734516dd438932
erikvw <ew2789@gmail.com>
2016-11-15 19:11:21 +0200

    simplify folders, update settings, urls, manage, wsgi files

--- edc_locator/example/models.py
+++
@@ -1,8 +0,0 @@
-from edc_base.model.models import BaseUuidModel
-from edc_locator.models import LocatorMixin
-
-
-class Locator(LocatorMixin, BaseUuidModel):
-
-    class Meta:
-        app_label = 'example'
