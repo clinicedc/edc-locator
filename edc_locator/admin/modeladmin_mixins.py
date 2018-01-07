e9167207f4a6612b3b5126d8b38b1b0277e876ca
erikvw <ew2789@gmail.com>
2018-01-07 16:42:43 +0200

    redirect to subject dashboard on save

--- edc_locator/admin/modeladmin_mixins.py
+++ edc_locator/admin/modeladmin_mixins.py
@@ -4,6 +4,8 @@ from edc_model_admin import (
     ModelAdminFormAutoNumberMixin, ModelAdminAuditFieldsMixin,
     ModelAdminReadOnlyMixin, ModelAdminInstitutionMixin,
     ModelAdminRedirectOnDeleteMixin)
+from django.urls.base import reverse
+from django.conf import settings
 
 
 class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsMixin,
@@ -14,3 +16,16 @@ class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructions
     list_per_page = 10
     date_hierarchy = 'modified'
     empty_value_display = '-'
+
+    post_url_on_delete_name = settings.DASHBOARD_URL_NAMES.get(
+        'subject_dashboard_url')
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return dict(subject_identifier=obj.subject_identifier)
+
+    def redirect_url(self, request, obj, post_url_continue=None):
+        if obj:
+            return reverse(settings.DASHBOARD_URL_NAMES.get(
+                'subject_dashboard_url'), kwargs=dict(subject_identifier=obj.subject_identifier))
+        else:
+            return super().redirect_url(request, obj, post_url_continue)
