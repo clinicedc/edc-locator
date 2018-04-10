8e89be3e591322d6db0678ee3fe6778d35d09852
erikvw <ew2789@gmail.com>
2018-04-10 20:38:25 +0200

    PEP8

--- edc_locator/view_mixins.py
+++ edc_locator/view_mixins.py
@@ -1,7 +1,7 @@
 from django.apps import apps as django_apps
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.base import ContextMixin
-from edc_action_item.site_action_items import  site_action_items
+from edc_action_item.site_action_items import site_action_items
 
 
 from .action_items import SUBJECT_LOCATOR_ACTION
