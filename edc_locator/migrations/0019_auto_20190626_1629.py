35546cf66e26120ed725c40639666d1769f14493
erikvw <ew2789@gmail.com>
2019-09-22 07:20:01 -0500

    remove verbose name from subject identifier

--- edc_locator/migrations/0019_auto_20190626_1629.py
+++ edc_locator/migrations/0019_auto_20190626_1629.py
@@ -5,19 +5,17 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('edc_locator', '0018_auto_20190305_0123'),
-    ]
+    dependencies = [("edc_locator", "0018_auto_20190305_0123")]
 
     operations = [
         migrations.AddField(
-            model_name='historicalsubjectlocator',
-            name='action_item_reason',
+            model_name="historicalsubjectlocator",
+            name="action_item_reason",
             field=models.TextField(editable=False, null=True),
         ),
         migrations.AddField(
-            model_name='subjectlocator',
-            name='action_item_reason',
+            model_name="subjectlocator",
+            name="action_item_reason",
             field=models.TextField(editable=False, null=True),
         ),
     ]
