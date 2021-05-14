016f8efe8e8c0a183baf549f8f16a11bdaaee0e2
erikvw <ew2789@gmail.com>
2021-05-14 16:20:59 -0500

    use edc admin site

--- edc_locator/admin_site.py
+++ edc_locator/admin_site.py
@@ -1,10 +1,5 @@
 from edc_model_admin.admin_site import EdcAdminSite
 
+from .apps import AppConfig
 
-class AdminSite(EdcAdminSite):
-    site_title = "Edc Locator"
-    site_header = "Edc Locator"
-    index_title = "Edc Locator"
-
-
-edc_locator_admin = AdminSite(name="edc_locator_admin")
+edc_locator_admin = EdcAdminSite(name="edc_locator_admin", app_label=AppConfig.name)
