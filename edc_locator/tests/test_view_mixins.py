daaaf730c8801576e83a42001dab69c28bdea4d0
erikvw <ew2789@gmail.com>
2022-09-24 22:36:16 -0500

    context mixin

--- edc_locator/tests/test_view_mixins.py
+++ edc_locator/tests/test_view_mixins.py
@@ -3,6 +3,7 @@ from unittest.case import skip
 from django.contrib.messages.storage.fallback import FallbackStorage
 from django.http.request import HttpRequest
 from django.test import TestCase
+from django.views.generic.base import ContextMixin
 from edc_registration.models import RegisteredSubject
 
 from ..view_mixins import SubjectLocatorViewMixin, SubjectLocatorViewMixinError
@@ -19,7 +20,7 @@ class TestViewMixins(TestCase):
         RegisteredSubject.objects.create(subject_identifier=self.subject_identifier)
 
     def test_subject_locator_raises_on_bad_model(self):
-        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin):
+        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin, ContextMixin):
             subject_locator_model_wrapper_cls = DummyModelWrapper
             subject_locator_model = "blah.blahblah"
 
@@ -27,14 +28,14 @@ class TestViewMixins(TestCase):
         self.assertRaises(SubjectLocatorViewMixinError, mixin.get_context_data)
 
     def test_subject_locator_raisesmissing_wrapper_cls(self):
-        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin):
+        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin, ContextMixin):
             subject_locator_model = "edc_locator.subjectlocator"
 
         self.assertRaises(SubjectLocatorViewMixinError, MySubjectLocatorViewMixin)
 
     @skip("problems emulating message framework")
     def test_mixin_messages(self):
-        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin):
+        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin, ContextMixin):
             subject_locator_model_wrapper_cls = DummyModelWrapper
             subject_locator_model = "edc_locator.subjectlocator"
 
@@ -47,7 +48,7 @@ class TestViewMixins(TestCase):
         self.assertGreater(len(mixin.request._messages._queued_messages), 0)
 
     def test_subject_locator_ok(self):
-        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin):
+        class MySubjectLocatorViewMixin(SubjectLocatorViewMixin, ContextMixin):
             subject_locator_model_wrapper_cls = DummyModelWrapper
             subject_locator_model = "edc_locator.subjectlocator"
 
