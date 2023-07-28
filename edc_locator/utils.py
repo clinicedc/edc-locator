f7dc05a17ce4596ed3b1dea5f3f4077e053fc2d3
erikvw <ew2789@gmail.com>
2023-07-27 21:31:20 -0500

    raise if more than one subject locator model specified

--- edc_locator/utils.py
+++ edc_locator/utils.py
@@ -10,19 +10,29 @@ if TYPE_CHECKING:
     from .model_mixins import LocatorModelMixin
 
 
-class LoctorModelError(Exception):
+class LocatorModelError(Exception):
     pass
 
 
 def get_locator_model(visit_schedule_name: str | None = None) -> str:
-    """Returns the locator model name in label_lower format"""
-    # TODO: is visit_schedule_name always available?? What if the
-    #  values are not the same?
-    #  (EDC_LOCATOR_LOCATOR_MODEL and visit_schedule.locator_model)??
+    """Returns the locator model name in label_lower format.
+
+    For now, cannot think of a case where more than one locator
+    model would be required.
+    Raises if visit_schedule declares a locator model != to
+    SUBJECT_LOCATOR_MODEL.
+    """
+    locator_model = None
     if visit_schedule_name:
         visit_schedule = site_visit_schedules.get_visit_schedule(visit_schedule_name)
-        return visit_schedule.locator_model
-    return getattr(settings, "EDC_LOCATOR_LOCATOR_MODEL", "edc_locator.subjectlocator")
+        locator_model = visit_schedule.locator_model
+    if locator_model and locator_model != get_locator_model():
+        raise LocatorModelError(
+            f"Ambiguous Locator model. See settings.SUBJECT_LOCATOR_MODEL and "
+            f"{visit_schedule_name}. Got `{settings.SUBJECT_LOCATOR_MODEL}` from "
+            f"settings and `{locator_model}` from {visit_schedule_name}."
+        )
+    return getattr(settings, "SUBJECT_LOCATOR_MODEL", "edc_locator.subjectlocator")
 
 
 def get_locator_model_cls(
@@ -31,7 +41,7 @@ def get_locator_model_cls(
     """Returns the Locator model class.
 
     Uses visit_schedule_name to get the class from the visit schedule
-    otherwise defaults to settings.EDC_LOCATOR_LOCATOR_MODEL.
+    otherwise defaults to settings.SUBJECT_LOCATOR_MODEL.
     """
     locator_model = locator_model or get_locator_model(visit_schedule_name)
     return django_apps.get_model(locator_model)
