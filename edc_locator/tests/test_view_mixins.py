9478c1897bdf2b9809139531621a16eb59312e89
erikvw <ew2789@gmail.com>
2023-12-18 22:04:46 -0600

    update tests

--- edc_locator/tests/test_view_mixins.py
+++ edc_locator/tests/test_view_mixins.py
@@ -1,17 +1,13 @@
 from unittest.case import skip
 
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
-from django.contrib.messages.storage.fallback import FallbackStorage
-from django.http.request import HttpRequest
 from django.test import TestCase
 from django.views.generic.base import ContextMixin
 from edc_action_item import site_action_items
 from edc_action_item.models import ActionItem
 from edc_registration.models import RegisteredSubject
-from edc_sites.utils import get_site_model_cls
 from edc_sites.view_mixins import SiteViewMixin
+from edc_test_utils.get_httprequest_for_tests import get_request_object_for_tests
+from edc_test_utils.get_user_for_tests import get_user_for_tests
 
 from ..action_items import SUBJECT_LOCATOR_ACTION
 from ..view_mixins import SubjectLocatorViewMixin, SubjectLocatorViewMixinError
@@ -24,30 +20,9 @@ class DummyModelWrapper:
 
 class TestViewMixins(TestCase):
     def setUp(self):
+        self.user = get_user_for_tests()
         self.subject_identifier = "12345"
         RegisteredSubject.objects.create(subject_identifier=self.subject_identifier)
-        self.user = get_user_model().objects.create_superuser(
-            "user_login", "u@example.com", "pass"
-        )
-        self.user.is_active = True
-        self.user.is_staff = True
-        self.user.save()
-        self.user.refresh_from_db()
-        self.user.userprofile.sites.add(get_site_model_cls().objects.get(id=settings.SITE_ID))
-        self.user.user_permissions.add(
-            Permission.objects.get(
-                codename="view_appointment", content_type__app_label="edc_appointment"
-            )
-        )
-
-    def get_request_object(self) -> HttpRequest:
-        request = HttpRequest()
-        setattr(request, "session", "session")
-        messages = FallbackStorage(request)
-        setattr(request, "_messages", messages)
-        setattr(request, "user", self.user)
-        setattr(request, "site", get_site_model_cls().objects.get(id=settings.SITE_ID))
-        return request
 
     def test_subject_locator_raises_on_bad_model(self):
         class MySubjectLocatorViewMixin(SiteViewMixin, SubjectLocatorViewMixin, ContextMixin):
@@ -56,7 +31,7 @@ class TestViewMixins(TestCase):
 
         mixin = MySubjectLocatorViewMixin()
         mixin.kwargs = {"subject_identifier": self.subject_identifier}
-        mixin.request = self.get_request_object()
+        mixin.request = get_request_object_for_tests(self.user)
         self.assertRaises(SubjectLocatorViewMixinError, mixin.get_context_data)
 
     def test_subject_locator_raisesmissing_wrapper_cls(self):
@@ -73,7 +48,7 @@ class TestViewMixins(TestCase):
 
         mixin = MySubjectLocatorViewMixin()
         mixin.kwargs = {"subject_identifier": self.subject_identifier}
-        mixin.request = self.get_request_object()
+        mixin.request = get_request_object_for_tests(self.user)
         self.assertGreater(len(mixin.request._messages._queued_messages), 0)
 
     def test_subject_locator_view_ok(self):
@@ -82,7 +57,7 @@ class TestViewMixins(TestCase):
             subject_locator_model = "edc_locator.subjectlocator"
 
         mixin = MySubjectLocatorViewMixin()
-        mixin.request = self.get_request_object()
+        mixin.request = get_request_object_for_tests(self.user)
         mixin.kwargs = {"subject_identifier": self.subject_identifier}
         try:
             mixin.get_context_data()
@@ -95,7 +70,7 @@ class TestViewMixins(TestCase):
             subject_locator_model = "edc_locator.subjectlocator"
 
         mixin = MySubjectLocatorViewMixin()
-        mixin.request = self.get_request_object()
+        mixin.request = get_request_object_for_tests(self.user)
         mixin.kwargs = {"subject_identifier": self.subject_identifier}
         try:
             mixin.get_context_data()
