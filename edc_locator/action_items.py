1e5f42262e5d960dd48c2bc36fde8fbb5fc3f88c
erikvw <ew2789@gmail.com>
2024-02-01 00:28:32 -0600

    use get_locator_model to access settings attr

--- edc_locator/action_items.py
+++ edc_locator/action_items.py
@@ -1,6 +1,8 @@
 from edc_action_item import Action, site_action_items
 from edc_constants.constants import HIGH_PRIORITY
 
+from edc_locator.utils import get_locator_model
+
 SUBJECT_LOCATOR_ACTION = "submit-subject-locator"
 
 
@@ -8,7 +10,7 @@ SUBJECT_LOCATOR_ACTION = "submit-subject-locator"
 class SubjectLocatorAction(Action):
     name = SUBJECT_LOCATOR_ACTION
     display_name = "Submit Subject Locator"
-    reference_model = "edc_locator.subjectlocator"
+    reference_model = get_locator_model()
     show_link_to_changelist = True
     admin_site_name = "edc_locator_admin"
     priority = HIGH_PRIORITY
